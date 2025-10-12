acbc5e3237fb9966976f6132bbd348ee4ff9a768
Phuc Nguyen <phuc.nguyen@siliconstack.com.au>
2025-10-12 16:43:33 +0700

    Longest Valid Parentheses problem

---
+++ LongestValidParentheseProblem.cs
@@ -0,0 +1,49 @@
+﻿namespace Leetcode.Algorithm
+{
+    public class LongestValidParentheseProblem
+    {
+        public int LongestValidParentheses(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return 0;
+
+            var dp = new bool[s.Length];
+
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if(s[i] == '(')
+                    stack.Push(i);
+                else
+                {
+                    if(stack.Count > 0)
+                    {
+                        var index = stack.Pop();
+                        dp[index] = true;
+                        dp[i] = true;
+                    }
+                }
+            }
+
+            var max = 0;
+            var current = 0;
+
+            for(var i = 0; i < s.Length; i++)
+            {
+                if (dp[i])
+                {
+                    current++;
+                }
+                else
+                {
+                    current = 0;
+                }
+
+                max = Math.Max(max, current);
+            }
+
+            return max;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,11 +4,9 @@ using Leetcode.Algorithm;
 
 var node = new ListNode(-10, new ListNode(-3, new ListNode(0, new ListNode(5, new ListNode(9)))));
 
-var service = new TakingMaximumEnergyProblem();
+var service = new LongestValidParentheseProblem();
 
-var energy = new int[] { 5, -10, 4, 3, 5, -9, 9, -7 };
-
-var result = service.MaximumEnergy(energy, 2);
+var max = service.LongestValidParentheses(")()())()()(");
 
 
 Console.WriteLine();
