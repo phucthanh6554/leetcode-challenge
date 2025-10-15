b0534cc007760858be4968744ca74db69c5fbd55
Phuc Nguyen <phuc.nguyen@siliconstack.com.au>
2025-10-15 14:58:06 +0700

    Adjacent Increasing Subarrays Detection I

---
+++ AdjacentIncresingSubArrayProblem.cs
@@ -0,0 +1,41 @@
+﻿namespace Leetcode.Algorithm
+{
+    public class AdjacentIncresingSubArrayProblem
+    {
+        public bool HasIncreasingSubarrays(IList<int> nums, int k)
+        {
+            if (nums.Count < k * 2)
+                return false;
+
+            var previousLength = 0;
+            var current = 1;
+
+            for (var i = 1; i < nums.Count; i++)
+            {
+                if (nums[i] > nums[i - 1])
+                {
+                    current++;
+
+                    if (current >= k + k)
+                        return true;
+                }
+                else
+                {
+                    if (previousLength >= k && current >= k)
+                        return true;
+
+                    previousLength = current;
+                    current = 1;
+                }
+            }
+
+            if (previousLength >= k && current >= k)
+                return true;
+
+            if(current >= k + k)
+                return true;
+
+            return false;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,9 +4,9 @@ using Leetcode.Algorithm;
 
 var node = new ListNode(-10, new ListNode(-3, new ListNode(0, new ListNode(5, new ListNode(9)))));
 
-var service = new FindResultantArrAfterRemoveAnagram();
+var service = new AdjacentIncresingSubArrayProblem();
 
-var max = service.RemoveAnagrams(new[] { "a","b","c","d","e"});
+var max = service.HasIncreasingSubarrays(new List<int> { -15, 19 }, 1);
 
 
 Console.WriteLine();
