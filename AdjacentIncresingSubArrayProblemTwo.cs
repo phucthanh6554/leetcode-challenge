f5a64030ce4de2abe61fc38e4a7941d79789df66
Phuc Nguyen <phuc.nguyen@siliconstack.com.au>
2025-10-16 10:29:20 +0700

    Adjacent Increasing Subarrays Detection II

---
+++ AdjacentIncresingSubArrayProblemTwo.cs
@@ -0,0 +1,35 @@
+﻿namespace Leetcode.Algorithm
+{
+    public class AdjacentIncresingSubArrayProblemTwo
+    {
+        public int MaxIncreasingSubarrays(IList<int> nums)
+        {
+            var previous = 0;
+            var current = 1;
+
+            var max = 0;
+
+            for (var i = 1; i < nums.Count; i++)
+            {
+                if (nums[i] > nums[i - 1])
+                {
+                    current++;
+
+                    max = Math.Max(max, current / 2);
+                }
+                else
+                {
+                    max = Math.Max(max, Math.Min(previous, current));
+
+                    previous = current;
+                    current = 1;
+                }
+            }
+
+            if(max < previous || max < current)
+                max = Math.Max(max, Math.Min(previous, current));
+
+            return max;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,10 @@
-﻿
-using System.Text.RegularExpressions;
-using Leetcode.Algorithm;
+﻿using Leetcode.Algorithm;
 
 var node = new ListNode(-10, new ListNode(-3, new ListNode(0, new ListNode(5, new ListNode(9)))));
 
-var service = new AdjacentIncresingSubArrayProblem();
+var service = new AdjacentIncresingSubArrayProblemTwo();
 
-var max = service.HasIncreasingSubarrays(new List<int> { -15, 19 }, 1);
+var max = service.MaxIncreasingSubarrays(new List<int> { 5, 8, -2, -1 });
 
 
 Console.WriteLine();
